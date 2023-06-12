1741a111a669c53367f3ad60c0655f8bd0239c95
Norornot <akbms99@gmail.com>
2023-06-12 08:12:40 -0700

    Day-9 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,3 +1,15 @@
+class CatalogModel {
+  static final items = [
+    Items(
+        "Product01",
+        "Iphone 12 pro max",
+        "osadsapojsadpadjadpd",
+        999,
+        "Colors.green",
+        "https://images.unsplash.com/photo-1607936854279-55e8a4c64888?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aXBob25lJTIwMTJ8ZW58MHx8MHx8fDA%3D&w=1000&q=80"),
+  ];
+}
+
 class Items {
   final String id;
   final String name;
@@ -8,8 +20,3 @@ class Items {
 
   Items(this.id, this.name, this.desc, this.price, this.color, this.imageUrl);
 }
-
-final products = [
-  Items("Product01", "Iphone 12 pro max", "osadsapojsadpadjadpd", 999,
-      "Colors.green", "dmsd;saldm"),
-];
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,26 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:myfirstapp/models/catalog.dart';
 import 'package:myfirstapp/widgets/drawer.dart';
+import 'package:myfirstapp/widgets/item_widget.dart';
 
-class Homepage extends StatefulWidget {
+class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
-  State<Homepage> createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage> {
-  @override
   Widget build(BuildContext context) {
-    int age = 19;
-    String name = "Bobby";
+    final dummyList = List.generate(12, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text("Catalog App")),
       ),
-      body: Center(
-        child: Container(
-          child: Text("My name is $name.I am ${age + 1} years old"),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Center(
+            child: ListView.builder(
+          itemBuilder: (context, index) {
+            return ItemsWidget(item: dummyList[index]);
+            ;
+          },
+          itemCount: dummyList.length,
+        )),
       ),
       drawer: MyDrawer(),
     );
